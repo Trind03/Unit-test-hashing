a5dc830e0ae840dc1e7c1f56fbf8ef5cda6a4b9b
Torstein <141224356+Trind03@users.noreply.github.com>
2024-02-27 09:54:50 +0100

    Refactoring by adding new lines

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,20 @@ TEST(Unit_sha256,sha256)
     EXPECT_EQ(_sha256,SHA256("Hello World"));
 }
 
-
 TEST(Unit_sha1,sha1)
 {
     SHA1 SHA1;
     std::string _sha1 = "0a4d55a8d778e5022fab701977c5d840bbc486d0";
     EXPECT_EQ(_sha1,SHA1("Hello World"));
 }
+
 TEST(Unit_sha3,sha3)
 {
     SHA3 SHA3;
     std::string _sha3 = "e167f68d6563d75bb25f3aa49c29ef612d41352dc00606de7cbd630bb2665f51";
     EXPECT_EQ(_sha3,SHA3("Hello World"));
 }
+
 TEST(Unit_md5,md5)
 {
     MD5 MD5;
