94c7726e7a7432983a9a8029f1366c296602adb5
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-25 18:51:59 +0100

    Added pointers for the rest of the tests/objects

--- src/tests.h
+++ src/tests.h
@@ -7,7 +7,6 @@
 #include <memory>
 TEST(Unit_sha256,sha256)
 {
-    //SHA256 SHA256;
     std::unique_ptr<SHA256>(sha256) = std::make_unique<SHA256>();
     std::string _sha256 = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e";
     EXPECT_EQ(_sha256,(*sha256)("Hello World"));
@@ -22,14 +21,14 @@ TEST(Unit_sha1,sha1)
 
 TEST(Unit_sha3,sha3)
 {
-    SHA3 SHA3;
+    std::unique_ptr<SHA3>(sha3) = std::make_unique<SHA3>();
     std::string _sha3 = "e167f68d6563d75bb25f3aa49c29ef612d41352dc00606de7cbd630bb2665f51";
-    EXPECT_EQ(_sha3,SHA3("Hello World"));
+    EXPECT_EQ(_sha3,(*sha3)("Hello World"));
 }
 
 TEST(Unit_md5,md5)
 {
-    MD5 MD5;
+    std::unique_ptr<MD5>(md5) = std::make_unique<MD5>();
     std::string _md5 = "b10a8db164e0754105b7a99be72e3fe5";
-    EXPECT_EQ(_md5,MD5("Hello World"));
+    EXPECT_EQ(_md5,(*md5)("Hello World"));
 }
