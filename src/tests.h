6b7570cbf146f686baae8b464230560f57ec25ec
Torstein <141224356+Trind03@users.noreply.github.com>
2024-05-08 11:12:58 +0200

    Update
    Added smart pointer support for all the tests

--- src/tests.h
+++ src/tests.h
@@ -18,35 +18,34 @@ TEST(Unit_sha256,sha256)
 TEST(Unit_sha1,sha1)
 {
     std::unique_ptr<SHA1>sha1 = std::make_unique<SHA1>();
-    SHA1 sha1;
     const char* _sha1 = "0a4d55a8d778e5022fab701977c5d840bbc486d0";
     EXPECT_EQ(_sha1,(*sha1)("Hello World"));
 }
 
 TEST(Unit_sha3,sha3)
 {
-    SHA3 sha3;
+    std::unique_ptr<SHA3>sha3 = std::make_unique<SHA3>();
     const char* _sha3 = "e167f68d6563d75bb25f3aa49c29ef612d41352dc00606de7cbd630bb2665f51";
-    EXPECT_EQ(_sha3,sha3("Hello World"));
+    EXPECT_EQ(_sha3,(*sha3)("Hello World"));
 }
 
 TEST(Unit_md5,md5)
 {
-    MD5 md5;
+    std::unique_ptr<MD5>md5 = std::make_unique<MD5>();
     const char* _md5 = "b10a8db164e0754105b7a99be72e3fe5";
-    EXPECT_EQ(_md5,md5("Hello World"));
+    EXPECT_EQ(_md5,(*md5)("Hello World"));
 }
 
 TEST(Unit_crc32,crc32)
 {
-    CRC32 crc32;
+    std::unique_ptr<CRC32>crc32 = std::make_unique<CRC32>();
     const char* _crc32 = "4a17b156";
-    EXPECT_EQ(_crc32,crc32("Hello World"));
+    EXPECT_EQ(_crc32,(*crc32)("Hello World"));
 }
 
 TEST(Unit_keccak,keccak)
 {
-    Keccak keccak;
+    std::unique_ptr<Keccak>keccak = std::make_unique<Keccak>();
     const char* _keccak = "592fa743889fc7f92ac2a37bb1f5ba1daf2a5c84741ca0e0061d243a2e6707ba";
-    EXPECT_EQ(_keccak,keccak("Hello World"));
+    EXPECT_EQ(_keccak,(*keccak)("Hello World"));
 }
